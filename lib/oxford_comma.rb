baa3b96b2a4d2270c5e8d4d0cc9ecd8c00b0b21c
shadi khaled <shadesoma@gmail.com>
2016-12-21 07:29:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+if array.size==1
+  array[0]
+elsif array.size==2
+  array.join(" and ")
+else
+  last=array.pop
+  array.join(", ")<<", and "<<last
 
 end
+end
+oxford_comma(["kiwi", "durian", "starfruit"])
